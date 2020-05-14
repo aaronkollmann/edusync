98d3017d87e819c83ab6b5d0a6f643da6656a4bd
A. Kollmann <aaron.kollmann@tuta.io>
2020-05-14 02:03:24 +0200

    download form is working but download isnt

--- src/main/java/de/ak/edusync/DataModel.java
+++ src/main/java/de/ak/edusync/DataModel.java
@@ -1,5 +1,7 @@
 package de.ak.edusync;
 
+import org.aarboard.nextcloud.api.NextcloudConnector;
+
 import java.io.Serializable;
 
 public class NextCloudAccountModel implements Serializable {
@@ -8,6 +10,7 @@ public class NextCloudAccountModel implements Serializable {
     private String username;
     private String password;
 
+    NextcloudConnector nextcloud = new NextcloudConnector(url, username, password);
 
     public String getUrl() {
         return url;
@@ -34,6 +37,10 @@ public class NextCloudAccountModel implements Serializable {
     }
 
     public NextCloudAccountModel() {
+    }
+
+    public void loginToNextcloud(){
+        NextcloudConnector nextcloud = new NextcloudConnector(url, username, password);
     }
 
 
