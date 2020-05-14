394ad566dfda6d6bb163b9f771d2d669f1929ba1
A. Kollmann <aaron.kollmann@tuta.io>
2020-05-14 17:22:44 +0200

    finished styling

--- src/main/java/de/ak/edusync/DataModel.java
+++ src/main/java/de/ak/edusync/DataModel.java
@@ -4,44 +4,61 @@ import org.aarboard.nextcloud.api.NextcloudConnector;
 
 import java.io.Serializable;
 
-public class NextCloudAccountModel implements Serializable {
+public class DataModel implements Serializable {
 
-    private String url;
-    private String username;
-    private String password;
 
-    NextcloudConnector nextcloud = new NextcloudConnector(url, username, password);
 
-    public String getUrl() {
-        return url;
+    public DataModel() {
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    private String nextCloudUrl;
+    private String username;
+    private String password;
+    private String fileUrl;
+
+    //DONT TOUCH GETTERS, IT WILL BREAK THYMELEAF!!!
+
+    public String getNextCloudUrl() {
+        return nextCloudUrl;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setNextCloudUrl(String nextCloudUrl) {
+        this.nextCloudUrl = nextCloudUrl;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public NextCloudAccountModel() {
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
     }
 
-    public void loginToNextcloud(){
-        NextcloudConnector nextcloud = new NextcloudConnector(url, username, password);
+    public NextcloudConnector loginToNextcloud(){
+        return new NextcloudConnector(nextCloudUrl, username, password);
     }
+
+    public String getFileName() {
+        String[] linkParts = fileUrl.split("/");
+        return linkParts[linkParts.length-1];
+    }
+
 
 
 }
--- src/main/java/de/ak/edusync/WebController.java
+++ src/main/java/de/ak/edusync/WebController.java
@@ -1,7 +1,20 @@
 package de.ak.edusync;
 
+import org.aarboard.nextcloud.api.NextcloudConnector;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class WebController {
@@ -9,4 +22,50 @@ public class WebController {
     public String generateWelcomePage(){
         return "index";
     }
+
+    @GetMapping("/app")
+    public String generateMainPage(Model model) {
+        model.addAttribute("dataObject", new DataModel());
+        return "app";
+    }
+
+    @PostMapping("/sync")
+    public String loginToNextCloud(@ModelAttribute DataModel dataObject) throws IOException {
+//        System.out.println(dataObject.getFileUrl()+ dataObject.getNextCloudUrl()+ dataObject.getPassword()+ dataObject.getUsername());
+
+
+        //login to nextcloud
+        NextcloudConnector nextcloud = dataObject.loginToNextcloud();
+
+        //get date time for versioning (just for testing)
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH_mm_ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        URL fileURL = new URL(dataObject.getFileUrl());
+        String localFilePath = "src/main/resources/" + dataObject.getFileName();
+        String remoteFolderPath = dtf.format(now);
+
+        //delete older versions in local repo if it exists
+        if(Files.exists(Paths.get(localFilePath))) {
+            Files.delete(Paths.get(localFilePath));
+        }
+
+        //download new file
+        InputStream in = new URL(dataObject.getFileUrl()).openStream();
+        Files.copy(in, Paths.get(localFilePath));
+
+        //put a folder with date and upload file into that folder
+        nextcloud.createFolder(remoteFolderPath);
+     //   nextcloud.uploadFile(, remoteFolderPath+"/"+dataObject.getFileName());
+
+
+        //forward to start page if everything went well
+        return "bye";
+    }
+
+
+
+
+
+
 }
